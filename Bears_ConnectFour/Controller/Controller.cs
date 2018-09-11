0839cd4197341111d7d8b77b221255ee4a2b79e4
grant121 <grant121@JB202-L21-W-D>
2018-09-11 13:21:55 -0400

    added more classes

--- Bears_ConnectFour/Controller/Controller.cs
+++ Bears_ConnectFour/Controller/Controller.cs
@@ -9,19 +9,46 @@ namespace Bears_ConnectFour
     class Controller
     {
         #region properties
+        private Board _board { get; set; }
+        private List<Piece> _pieces { get; set; }
+        private Config _config { get; set; }
 
         #endregion
 
         #region constructors
         public Controller()
         {
-            Board _board = new Board();
-            _board.PrintGrid();
+            InstantiateConfig();
+            InstantiateBoard();
+            InstantiatePieces(2);
             Console.ReadLine();
         }
         #endregion
 
         #region methods
+        private void InstantiateConfig()
+        {
+            _config = new Config();
+        }
+
+        private void InstantiateBoard()
+        {
+            _board = new Board();
+        }
+
+        private void InstantiatePieces(int players)
+        {
+            Piece p;
+            for (int i = 0; i < players; i++)
+            {
+                p = new Piece()
+                {
+                    Id = i
+                    
+                }   
+                _pieces.Add(p);
+            }
+        }
 
         //main menu 
         //calls game loop
---
+++ Bears_ConnectFour/Model/Config.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bears_ConnectFour
+{
+    public class Config
+    {
+        #region properties
+        public int Players { get; set; }
+        #endregion
+
+        #region constructors
+        public Config()
+        {
+
+        }
+        #endregion
+    }
+}
